218347e5fa06ec766bfd30599e0f4fb93ce07356
Fawei <50777393@qq.com>
2025-07-06 13:19:46 +0800

    leet code - stale server

---
+++ LeetCode/logic/StaleServer.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using Utils.Enumerable;
+
+namespace LeetCode.logic
+{
+    public class StaleServer
+    {
+        public StaleServer() 
+        {
+            // 测试用例
+            int n = 5;
+            List<List<int>> log_data = new List<List<int>>
+            {
+                new List<int> { 1, 1 },
+                new List<int> { 2, 2 },
+                new List<int> { 3, 3 },
+                new List<int> { 4, 4 },
+                new List<int> { 5, 5 },
+                new List<int> { 1, 6 },
+                new List<int> { 2, 7 }
+            };
+            List<int> query = new List<int> { 6, 7 };
+            int x = 2;
+            var result = getStaleServerCount(n, log_data, query, x);
+            Debug.WriteLine(result.NotNullString());
+        }
+
+        public List<int> getStaleServerCount(int n, List<List<int>> log_data, List<int> query, int x)
+        {
+            List<int> results = new List<int>();
+
+            foreach (int q in query)
+            {
+                int start = q - x;
+                int end = q;
+                HashSet<int> activeServers = new HashSet<int>();
+
+                foreach (List<int> entry in log_data)
+                {
+                    int serverId = entry[0];
+                    int time = entry[1];
+
+                    if (time >= start && time <= end)
+                    {
+                        activeServers.Add(serverId);
+                    }
+                }
+
+                // 未收到请求的服务器数量 = 总服务器数 - 有请求的服务器数
+                results.Add(n - activeServers.Count);
+            }
+
+            return results;
+        }
+    }
+}
