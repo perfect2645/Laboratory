ca2b78fb39c67599fc5fe9329cb10e3270eedecb
Fawei <50777393@qq.com>
2025-07-15 17:50:20 +0800

    wpf tab control

---
+++ WpfControls/LogConsole/LogPanel.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace WpfControls.LogConsole
+{
+    /// <summary>
+    /// LogPanel.xaml 的交互逻辑
+    /// </summary>
+    public partial class LogPanel : UserControl
+    {
+        private static readonly Type ControlType = typeof(LogPanel);
+
+        private static readonly object _lock = new object();
+
+        public Action<string> WriteLogAction { get; private set; }
+        public Func<string> GetLogAction { get; private set; }
+
+        public LogPanel()
+        {
+            InitializeComponent();
+            WriteLogAction = new Action<string>(WriteLine);
+            GetLogAction = new Func<string>(GetRichText);
+        }
+
+        private void logText_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #region Write
+        private void WriteLine(string text)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                Clear();
+                var paragraph = new Paragraph();
+                paragraph.Inlines.Add(new Run(text));
+                document.Blocks.Add(paragraph);
+                logText.ScrollToEnd();
+            });
+        }
+
+        #endregion Write
+
+        #region Read
+
+        public string GetRichText()
+        {
+            if (logText.Document?.ContentStart == null)
+            {
+                return string.Empty;
+            }
+            TextRange textRange = new TextRange(logText.Document.ContentStart, logText.Document.ContentEnd);
+            return textRange?.Text ?? string.Empty;
+        }
+
+        #endregion Read
+
+        #region Clear
+
+        public void Clear()
+        {
+            if (logText.Document.Blocks.Count > 300)
+            {
+                logText.Document.Blocks.Clear();
+            }
+        }
+
+        #endregion Clear
+    }
+}
---
+++ WpfControls/View/BaseConsole.xaml.cs
@@ -0,0 +1,61 @@
+﻿using WpfControls.LogConsole;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfControls.View
+{
+    /// <summary>
+    /// BaseConsole.xaml 的交互逻辑
+    /// </summary>
+    public partial class BaseConsole : UserControl
+    {
+        #region Properties
+
+        private static readonly Type ControlType = typeof(BaseConsole);
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), ControlType, new PropertyMetadata("Console with Logging"));
+
+        public LogPanel LogPanel
+        {
+            get { return (LogPanel)GetValue(LogPanelProperty); }
+            set { SetValue(LogPanelProperty, value); }
+        }
+
+        public static readonly DependencyProperty LogPanelProperty =
+            DependencyProperty.Register("LogPanel", typeof(LogPanel), ControlType);
+
+        public StackPanel ConsoleContent
+        {
+            get { return (StackPanel)GetValue(ConsoleContentProperty); }
+            set { SetValue(ConsoleContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty ConsoleContentProperty =
+            DependencyProperty.Register("ConsoleContent", typeof(StackPanel), ControlType);
+
+        #endregion Properties
+
+        #region Constructor
+
+        public BaseConsole()
+        {
+            InitializeComponent();
+            LogPanel = logPanelCtrl;
+        }
+
+        #endregion Constructor
+
+        #region events
+
+        #endregion events
+    }
+}
