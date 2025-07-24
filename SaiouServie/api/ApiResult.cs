a6d6cfd726b43ede747de1d4696c4a95e06533c0
Fawei <50777393@qq.com>
2025-07-24 13:04:42 +0800

    saiou api result

---
+++ SaiouServie/api/ApiResult.cs
@@ -0,0 +1,19 @@
+﻿namespace SaiouService.api
+{
+    public class ApiResult<T>
+    {
+        private int code;
+        public int Code { get { return code; } set { code = value; } }
+
+
+        private string msg;
+        public string Msg { get { return msg; } set { msg = value; } }
+
+        private T data;
+        public T Data { get { return data; } set { data = value; } }
+
+        private bool success;
+        public bool Success { get { return success; } set { success = value; } }
+
+    }
+}
