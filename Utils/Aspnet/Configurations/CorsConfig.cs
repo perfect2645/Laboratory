edca02a737b67560a76f095a199474112f86f313
Fawei <50777393@qq.com>
2025-12-12 01:14:29 +0800

    webapi- network config

---
+++ Utils/Aspnet/Configurations/CorsConfig.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Utils.Aspnet.Configurations
+{
+    public static class CorsConfig
+    {
+        public static void AllowCorsExt(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("All", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowCredentials(); // 如果前端发送凭据（如cookies）
+                });
+                options.AddPolicy("AllowReactApp", policy =>
+                {
+                    policy.WithOrigins("http://localhost:3000")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowCredentials(); // 如果前端发送凭据（如cookies）
+                });
+            });
+        }
+        public static void UseCorsExt(this IApplicationBuilder app)
+        {
+            app.UseCors("All");
+        }
+    }
+}
