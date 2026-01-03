75ee8bba068949442e78c0fee4f65cf803d158ff
Fawei <50777393@qq.com>
2026-01-03 18:38:21 +0800

    utils - register services

---
+++ Utils/Aspnet/Configurations/RouteConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Utils.Aspnet.Configurations
+{
+    public static class RouteConfig
+    {
+        public static IServiceCollection ConfigureRoutes(this IServiceCollection services)
+        {
+            services.AddRouting(options =>
+            {
+                options.LowercaseUrls = true;
+                options.LowercaseQueryStrings = true;
+                options.AppendTrailingSlash = false;
+            });
+            return services;
+        }
+    }
+}
---
+++ Utils/Aspnet/Configurations/ServicesRegister.cs
@@ -0,0 +1,39 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Utils.Aspnet.Filters;
+using Utils.Ioc;
+
+namespace Utils.Aspnet.Configurations
+{
+    public static class ServicesRegister
+    {
+        public static void RegisterCommonServices(this WebApplicationBuilder builder)
+        {
+            //builder.UseAutofac();
+            builder.Services.AddHttpContextAccessor();
+            builder.Services.AddControllers(options =>
+            {
+                options.Filters.Add<GlobalExceptionFilter>();
+            });
+
+            builder.Services.ConfigureRoutes();
+        }
+
+        public static void RegisterAssembliesAutofac(this WebApplicationBuilder builder, Assembly[] assemblies)
+        {
+            builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+            builder.Host.ConfigureContainer<ContainerBuilder>((context, containerBuilder) =>
+            {
+                containerBuilder.RegisterModule(new AutoRegisterModule(
+                    assemblies
+                //Assembly.GetExecutingAssembly(),
+                //typeof(IShirtRepository).Assembly
+                ));
+            });
+        }
+    }
+}
