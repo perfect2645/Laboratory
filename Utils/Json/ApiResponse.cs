6ad46f94de62ca67b2c2e6ed97f7fbb7571c88f6
Fawei <50777393@qq.com>
2026-01-07 21:53:14 +0800

    utils - update api response

--- Utils/Json/ApiResponse.cs
+++ Utils/Json/ApiResponse.cs
@@ -2,10 +2,19 @@
 {
     public class ApiResponse<T>
     {
-        public T Data { get; init; }
-        public bool Success { get; init; } = true;
+        public required T? Data { get; init; }
+        public required int Code { get; init; }
         public string? Message { get; init; }
+        public bool IsSuccess => Code == 200;
 
-        public ApiResponse(T data) => Data = data;
+        public static ApiResponse<T> Success(T? data, string message = "Success")
+        {
+            return new ApiResponse<T> { Code = 200, Message = message, Data = data };
+        }
+
+        public static ApiResponse<T> Fail(string message = "Failed", int code = 500)
+        {
+            return new ApiResponse<T> { Code = code, Message = message, Data = default };
+        }
     }
 }
