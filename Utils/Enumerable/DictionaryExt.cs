2f919a82dd9cac3b2307983db51ce6e909b882ae
Fawei <50777393@qq.com>
2026-01-22 22:40:26 +0800

    utils-1.3.0-upgrade DictionaryExt using net10 extension

--- Utils/Enumerable/DictionaryExt.cs
+++ Utils/Enumerable/DictionaryExt.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
 using System.Text.Json;
 using Utils.Generic;
 using Utils.Json;
@@ -7,216 +8,164 @@ namespace Utils.Enumerable
 {
     public static class DictionaryExt
     {
-        private static object UpdateLock = new object();
-
         #region Dictionary
 
-        public static void AddOrUpdate<TKey, TValue>(this Dictionary<TKey, TValue> dic, TKey key, TValue value) where TKey : notnull
+        private static readonly ConditionalWeakTable<object, object> _lockTable = new();
+
+        extension<TKey, TValue>(Dictionary<TKey, TValue> dic) where TKey : notnull
         {
-            lock (UpdateLock)
+            public void AddOrUpdate(TKey key, TValue value)
             {
-                dic = dic ?? new Dictionary<TKey, TValue>();
-                if (dic.ContainsKey(key))
+                ArgumentNullException.ThrowIfNull(dic, nameof(dic));
+                ArgumentNullException.ThrowIfNull(key, nameof(key));
+
+                // thread-safe update
+                object lockObj = _lockTable.GetValue(dic, _ => new object());
+                lock (lockObj)
                 {
                     dic[key] = value;
                 }
-                else
+            }
+
+            public void AddOrUpdate(Dictionary<TKey, TValue> dicToAdd)
+            {
+                ArgumentNullException.ThrowIfNull(dic, nameof(dic));
+
+                if (dicToAdd == null || dicToAdd.Count == 0)
                 {
-                    dic.Add(key, value);
+                    return;
+                }
+
+                object lockObj = _lockTable.GetValue(dic, _ => new object());
+
+                lock (lockObj)
+                {
+                    foreach ((TKey key, TValue value) in dicToAdd)
+                    {
+                        dic[key] = value;
+                    }
                 }
             }
-        }
 
-        public static void AddOrUpdate<TKey, TValue>(this Dictionary<TKey, TValue> dic, Dictionary<TKey, TValue> dicToAdd) where TKey : notnull
-        {
-            dic = dic ?? new Dictionary<TKey, TValue>();
-
-            if (dicToAdd == null)
+            public bool HasItem()
             {
-                return;
-            }
-
-            foreach (var pair in dicToAdd)
-            {
-                dic.AddOrUpdate(pair.Key, pair.Value);
-            }
-        }
-
-        public static void AddOrUpdate<TKey, TValue>(this Dictionary<TKey, TValue> dic, Dictionary<TKey, TValue> source, TKey key) where TKey : notnull
-        {
-            lock (UpdateLock)
-            {
-                dic = dic ?? new Dictionary<TKey, TValue>();
-                if (dic.ContainsKey(key))
+                if (dic == null)
                 {
-                    dic[key] = source[key];
+                    return false;
                 }
-                else
+
+                return dic.Count > 0;
+            }
+        }
+
+
+        extension(Dictionary<string, object> dic)
+        {
+            public string ToJson()
+            {
+                var json = JsonSerializer.Serialize(dic, JsonEncoder.JsonOption);
+                return json;
+            }
+
+            public string? GetString(string key)
+            {
+                if (dic == null || key == null)
                 {
-                    dic.Add(key, source[key]);
+                    return null;
                 }
+
+                if (!dic.ContainsKey(key))
+                {
+                    return null;
+                }
+
+                if (dic[key] == null)
+                {
+                    return null;
+                }
+                return dic[key].ToString();
             }
-        }
 
-
-        public static string ToJson(this Dictionary<string, object> dic)
-        {
-            var json = JsonSerializer.Serialize(dic, JsonEncoder.JsonOption);
-
-            return json;
-        }
-
-        public static string ToJson(this Dictionary<string, string> dic)
-        {
-            var json = JsonSerializer.Serialize(dic, JsonEncoder.JsonOption);
-
-            return json;
-        }
-
-        public static bool HasItem(this Dictionary<string, object> dic)
-        {
-            if (dic == null)
+            public int GetInt(string key)
             {
-                return false;
+                var strValue = dic.GetString(key);
+                return strValue.ToInt();
             }
 
-            return dic.Count > 0;
-        }
-
-        public static string? GetString(this Dictionary<string, object> dic, string key)
-        {
-            if (dic == null || key == null)
+            public object? GetValue(string key)
             {
-                return null;
-            }
+                if (dic == null || key == null)
+                {
+                    return null;
+                }
 
-            if (!dic.ContainsKey(key))
-            {
-                return null;
+                if (!dic.ContainsKey(key))
+                {
+                    return null;
+                }
+                return dic[key];
             }
-
-            if (dic[key] == null)
-            {
-                return null;
-            }
-            return dic[key].ToString();
-        }
-
-        public static int GetInt(this Dictionary<string, object> dic, string key)
-        {
-            var strValue = dic.GetString(key);
-            return strValue.ToInt();
-        }
-
-        public static object? GetValue(this Dictionary<string, object> dic, string key)
-        {
-            if (dic == null || key == null)
-            {
-                return null;
-            }
-
-            if (!dic.ContainsKey(key))
-            {
-                return null;
-            }
-            return dic[key];
         }
 
         #endregion Dictionary
 
         #region ConcurrentDictionary
 
-        public static void AddOrUpdate<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> dic, TKey key, TValue value) where TKey : notnull
+        extension<TKey, TValue>(ConcurrentDictionary<TKey, TValue> dic) where TKey : notnull
         {
-            lock (UpdateLock)
+            public void AddOrUpdate(TKey key, TValue value)
             {
-                dic = dic ?? new ConcurrentDictionary<TKey, TValue>();
-                if (dic.ContainsKey(key))
+                ArgumentNullException.ThrowIfNull(dic, nameof(dic));
+                ArgumentNullException.ThrowIfNull(key, nameof(key));
+                dic.AddOrUpdate(key, value, (_, _) => value);
+            }
+        }
+
+        extension(ConcurrentDictionary<string, object> dic)
+        {
+            public string ToJson()
+            {
+                var json = JsonSerializer.Serialize(JsonEncoder.JsonOption);
+                return json;
+            }
+
+            public bool HasItem()
+            {
+                if (dic == null)
                 {
-                    dic[key] = value;
+                    return false;
                 }
-                else
+
+                return dic.Count > 0;
+            }
+
+            public string? GetString(string key)
+            {
+                if (dic == null || key == null)
                 {
-                    dic.TryAdd(key, value);
+                    return null;
                 }
-            }
-        }
 
-        public static void AddOrUpdate<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> dic, ConcurrentDictionary<TKey, TValue> dicToAdd) where TKey : notnull
-        {
-            dic = dic ?? new ConcurrentDictionary<TKey, TValue>();
-
-            if (dicToAdd == null)
-            {
-                return;
-            }
-
-            foreach (var pair in dicToAdd)
-            {
-                dic.AddOrUpdate(pair.Key, pair.Value);
-            }
-        }
-
-        public static void AddOrUpdate<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> dic, ConcurrentDictionary<TKey, TValue> source, TKey key) where TKey : notnull
-        {
-            lock (UpdateLock)
-            {
-                dic = dic ?? new ConcurrentDictionary<TKey, TValue>();
-                if (dic.ContainsKey(key))
+                if (!dic.ContainsKey(key))
                 {
-                    dic[key] = source[key];
+                    return null;
                 }
-                else
+                return dic[key].ToString();
+            }
+
+            public object? GetValue(string key)
+            {
+                if (dic == null || key == null)
                 {
-                    dic.TryAdd(key, source[key]);
+                    return null;
                 }
+
+                if (!dic.ContainsKey(key))
+                {
+                    return null;
+                }
+                return dic[key];
             }
-        }
-
-
-        public static string ToJson(this ConcurrentDictionary<string, object> dic)
-        {
-            var json = JsonSerializer.Serialize(dic, JsonEncoder.JsonOption);
-
-            return json;
-        }
-
-        public static bool HasItem(this ConcurrentDictionary<string, object> dic)
-        {
-            if (dic == null)
-            {
-                return false;
-            }
-
-            return dic.Count > 0;
-        }
-
-        public static string? GetString(this ConcurrentDictionary<string, object> dic, string key)
-        {
-            if (dic == null || key == null)
-            {
-                return null;
-            }
-
-            if (!dic.ContainsKey(key))
-            {
-                return null;
-            }
-            return dic[key].ToString();
-        }
-
-        public static object? GetValue(this ConcurrentDictionary<string, object> dic, string key)
-        {
-            if (dic == null || key == null)
-            {
-                return null;
-            }
-
-            if (!dic.ContainsKey(key))
-            {
-                return null;
-            }
-            return dic[key];
         }
 
         #endregion ConcurrentDictionary
